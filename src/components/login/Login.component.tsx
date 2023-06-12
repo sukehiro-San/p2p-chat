9a53ddc5482a1f027da09e96db0c76c66704b6e7
Sunny Kewat <sunnykewat99@gmail.com>
2023-06-12 15:27:22 +0530

    Init Commit

---
+++ src/components/login/Login.component.tsx
@@ -0,0 +1,40 @@
+import "Login.styles.css";
+
+const LoginComponent = () => {
+  return (
+    <div>
+      <section>
+        <div className="color"></div>
+        <div className="color"></div>
+        <div className="color"></div>
+        <div className="box">
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="container">
+            <div className="form">
+              <h2>Get In</h2>
+              <form>
+                <div className="inputBox">
+                  <input type="text" placeholder="Username" />
+                </div>
+                <div className="inputBox">
+                  <input type="submit" value=" Login " />
+
+                  <input type="reset" value=" Reset " />
+                </div>
+                <p className="forget">
+                  Forgot Account?<a href="#"> Click Here</a>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default LoginComponent;
